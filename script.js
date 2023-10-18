bbd91a476f67518416c0e678e649c345440e96f9
tuanhoang04 <hoangngoctuan2611@gmail.com>
2023-10-18 11:01:44 +0700

    Biggest update

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const items = document.querySelectorAll('.accordion-item');
+// document.addEventListener("DOMContentLoaded", function() {
+//     const items = document.querySelectorAll('.accordion-item');
 
-    // Initially display the first content
-    const firstItem = items[0];
-    const firstContent = firstItem.querySelector('.content');
-    firstContent.style.display = 'block';
+//     // Initially display the first content
+//     const firstItem = items[0];
+//     const firstContent = firstItem.querySelector('.content');
+//     firstContent.style.display = 'block';
 
-    items.forEach(item => {
-        const title = item.querySelector('.title');
-        title.addEventListener('click', () => {
-            items.forEach(otherItem => {
-                if (otherItem !== item) {
-                    otherItem.querySelector('.content').style.display = 'none';
-                }
-            });
-            const content = item.querySelector('.content');
-            content.style.display = content.style.display === 'none' ? 'block' : 'none';
-        });
-    });
-});
-  
+//     items.forEach(item => {
+//         const title = item.querySelector('.title');
+//         title.addEventListener('click', () => {
+//             items.forEach(otherItem => {
+//                 if (otherItem !== item) {
+//                     otherItem.querySelector('.content').style.display = 'none';
+//                 }
+//             });
+//             const content = item.querySelector('.content');
+//             content.style.display = content.style.display === 'none' ? 'block' : 'none';
+//         });
+//     });
+// });
